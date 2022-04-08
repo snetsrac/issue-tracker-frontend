223a7cb623fb801acde46b33edae2737ac882493
snetsrac <dev@chriscarstens.com>
2022-04-08 21:34:18 +0000

    Refactor table

--- components/table.tsx
+++ components/table.tsx
@@ -11,8 +11,6 @@ type TableData = {
 };
 
 type TableProps = {
-  title: string;
-  description: string;
   columns: TableColumn[];
   data: TableData[];
 };
@@ -35,68 +33,47 @@ function columnPadding(length: number, i: number) {
   return padding;
 }
 
-export default function Table({
-  title,
-  description,
-  columns,
-  data,
-}: TableProps) {
+export default function Table({ columns, data }: TableProps) {
   return (
-    <div className='px-4 sm:px-6 lg:px-8'>
-      <div className='sm:flex sm:items-center'>
-        <div className='sm:flex-auto'>
-          <h1 className='text-xl font-semibold text-gray-900'>{title}</h1>
-          <p className='mt-2 text-sm text-gray-700'>{description}</p>
-        </div>
-        <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
-          <button
-            type='button'
-            className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'
-          >
-            Button text
-          </button>
-        </div>
-      </div>
-      <div className='mt-8 flex flex-col'>
-        <div className='-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8'>
-          <div className='inline-block min-w-full py-2 align-middle'>
-            <div className='overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5'>
-              <table className='min-w-full divide-y divide-gray-300'>
-                <thead className='bg-gray-50'>
-                  <tr>
+    <div className='mt-8 flex flex-col'>
+      <div className='-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+        <div className='inline-block min-w-full py-2 align-middle'>
+          <div className='overflow-hidden shadow-sm ring-1 ring-black ring-opacity-5'>
+            <table className='min-w-full divide-y divide-gray-300'>
+              <thead className='bg-gray-50'>
+                <tr>
+                  {columns.map((column, i) => (
+                    <th
+                      key={column.accessor}
+                      scope='col'
+                      className={classNames(
+                        'py-3.5 text-left text-sm font-semibold text-gray-900',
+                        columnPadding(columns.length, i)
+                      )}
+                    >
+                      {column.title}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className='divide-y divide-gray-200 bg-white'>
+                {data.map((row) => (
+                  <tr key={row.id} className='hover:bg-gray-200'>
                     {columns.map((column, i) => (
-                      <th
+                      <td
                         key={column.accessor}
-                        scope='col'
                         className={classNames(
-                          'py-3.5 text-left text-sm font-semibold text-gray-900',
+                          'max-w-0 truncate py-4 text-sm text-gray-500',
                           columnPadding(columns.length, i)
                         )}
                       >
-                        {column.title}
-                      </th>
+                        {row[column.accessor]}
+                      </td>
                     ))}
                   </tr>
-                </thead>
-                <tbody className='divide-y divide-gray-200 bg-white'>
-                  {data.map((row) => (
-                    <tr key={row.id} className='hover:bg-gray-200'>
-                      {columns.map((column, i) => (
-                        <td
-                          key={column.accessor}
-                          className={classNames(
-                            'max-w-0 truncate py-4 text-sm text-gray-500',
-                            columnPadding(columns.length, i)
-                          )}
-                        >
-                          {row[column.accessor]}
-                        </td>
-                      ))}
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
--- pages/issues/index.tsx
+++ pages/issues/index.tsx
@@ -59,12 +59,25 @@ export default function IssuesPage() {
   });
 
   return (
-    <Table
-      title='Issues'
-      description='A list of all the issues in the database (for now) including their title and description'
-      columns={columns}
-      data={issues}
-    />
+    <div className='px-4 sm:px-6 lg:px-8'>
+      <div className='sm:flex sm:items-center'>
+        <div className='sm:flex-auto'>
+          <h1 className='text-xl font-semibold text-gray-900'>Issues</h1>
+          <p className='mt-2 text-sm text-gray-700'>
+            A list of all the issues in the database (for now) including their
+            title and description.
+          </p>
+        </div>
+        <div className='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
+          <Link href='/issues/new'>
+            <a className='inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto'>
+              Create Issue
+            </a>
+          </Link>
+        </div>
+      </div>
+      <Table columns={columns} data={issues} />
+    </div>
   );
 }
 
